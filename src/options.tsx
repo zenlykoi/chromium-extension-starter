9eb60d89090e014b8cf3f678ba43fe4c726e2892
My Name <my-name@chromium.org>
2023-02-23 11:51:34 +0700

    update

--- src/options.tsx
+++ src/options.tsx
@@ -1,1 +1,18 @@
-console.log('This is option page');
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Option = () => {
+  return (
+    <>
+      <h4>Chromium extension starter option page</h4>
+      <i>Author: <a href="https://www.facebook.com/nguyenphuong.2661999/" target={'_blank'}>NguyenPhuong99</a></i>
+    </>
+  );
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Option />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,1 +1,18 @@
-console.log('This is popup');
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Popup = () => {
+  return (
+    <>
+      <h4>Chromium extension starter popup</h4>
+      <i>Author: <a href="https://www.facebook.com/nguyenphuong.2661999/" target={'_blank'}>NguyenPhuong99</a></i>
+    </>
+  );
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Popup />
+  </React.StrictMode>,
+  document.getElementById("root")
+);

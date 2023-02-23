7e36760906e51a7bf3e7182971a20ac5ffc2bcbc
My Name <my-name@chromium.org>
2023-02-23 15:03:07 +0700

    add obfuscator

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,6 +1,27 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const WebpackObfuscator = require('webpack-obfuscator');
 
-module.exports = merge(common, {
-    mode: 'production'
-});
+module.exports = function(env) {
+	const isObfuscate = env.obfuscate ? true : false;
+
+	const config = {
+		mode: 'production'
+	};
+	
+	if (isObfuscate) {
+		config.plugins = [
+			new WebpackObfuscator({
+				disableConsoleOutput: true,
+				selfDefending: true,
+				debugProtection: true,
+				debugProtectionInterval: 4000,
+				stringArrayIndexesType: [
+					'hexadecimal-number'
+				]
+			})
+		]
+	}
+
+	return merge(common, config);
+};
